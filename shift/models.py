443c9ac48429bb10eae38cb45c67543512d8af69
Fumi <52829191+Te-kito@users.noreply.github.com>
2020-09-17 21:27:57 +0900

    nokositoku

--- shift/models.py
+++ shift/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 class Shift(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='ユーザー', on_delete=models.SET_NULL, blank=True, null=True)
     date = models.DateField('日付')
-    is_work = models.NullBooleanField(verbose_name='出勤', default=False)
+    is_work = models.BooleanField(verbose_name='出勤', default=False, null=True)
     all_day = models.BooleanField(verbose_name='終日', default=False)
     start_time = models.TimeField(verbose_name='開始時間',default=datetime.time(8,0))
     end_time = models.TimeField(verbose_name='終了時刻',default=datetime.time(17,0))
